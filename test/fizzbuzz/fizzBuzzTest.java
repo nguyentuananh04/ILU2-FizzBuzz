b4505d7651287c715f877bc704af24cbbdaf5152
nguyentuananh04 <tanh22012004@gmail.com>
2024-11-29 02:11:53 +0100

    initialiser class, test

---
+++ test/fizzbuzz/fizzBuzzTest.java
@@ -0,0 +1,14 @@
+package fizzbuzz;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class fizzBuzzTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}

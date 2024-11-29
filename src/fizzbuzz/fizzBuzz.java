2db96e39b38238a0c1072c51ca5dfaf859a5b6b9
nguyentuananh04 <tanh22012004@gmail.com>
2024-11-29 02:26:08 +0100

    testfizzun

--- src/fizzbuzz/fizzBuzz.java
+++ src/fizzbuzz/fizzBuzz.java
@@ -1,5 +1,7 @@
 package fizzbuzz;
 
 public class fizzBuzz {
-
+	String fizzbuzz(int i) {
+		return "1";
+	}
 }
--- test/fizzbuzz/fizzBuzzTest.java
+++ test/fizzbuzz/fizzBuzzTest.java
@@ -2,13 +2,20 @@ package fizzbuzz;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class fizzBuzzTest {
-
+	fizzBuzz fizzBuzz ;
+	
+	@BeforeEach
+	void initFizzBuzz() {
+		fizzBuzz = new fizzBuzz();
+	}
+	
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void fizzUn() {
+		assertEquals(fizzBuzz.fizzbuzz(1), "1");
 	}
 
 }
